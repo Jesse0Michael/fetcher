dc569ff8bd3101a5d3e2fb8ff7943e9bd6f91455
Jesse Michael <jesse.michael1234@gmail.com>
2020-07-05 19:21:33 -0700

    fix: fix lambda support

--- pkg/lambda/main.go
+++ pkg/lambda/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dghubble/go-twitter/twitter"
 	fetcher "github.com/jesse0michael/fetcher/pkg/fetcher"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 )
 
@@ -17,7 +20,14 @@ type FeedIDs struct {
 }
 
 // HandleRequest is the lambda entrypoint for fetching feeds.
-func HandleRequest(ctx context.Context, feeds FeedIDs) (interface{}, error) {
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (interface{}, error) {
+	// parse feed IDs
+	var feeds FeedIDs
+	if err := json.Unmarshal([]byte(req.Body), &feeds); err != nil {
+		return nil, errors.New("failed to parse API Gateway request")
+	}
+	fmt.Println(feeds.Twitter)
+
 	// oauth2 configures a client that uses app credentials to keep a fresh token
 	config := &clientcredentials.Config{
 		ClientID:     os.Getenv("TWITTER_CLIENT_KEY"),
@@ -25,7 +35,7 @@ func HandleRequest(ctx context.Context, feeds FeedIDs) (interface{}, error) {
 		TokenURL:     "https://api.twitter.com/oauth2/token",
 	}
 	// http.Client will automatically authorize Requests
-	httpClient := config.Client(oauth2.NoContext)
+	httpClient := config.Client(context.Background())
 
 	// Twitter client
 	twitterClient := twitter.NewClient(httpClient)
